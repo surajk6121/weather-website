7a388cabba996da98ec6dc5397e98da0a82ca499
suraj kumar <suraj.kumar5@bfhl.in>
2021-02-05 16:12:12 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ weatherForm.addEventListener('submit',(e)=>{
     console.log(search.value)
     messageone.textContent='Loading...'
     messagetwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+search.value).then((response)=>{
+    fetch('/weather?address='+search.value).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
